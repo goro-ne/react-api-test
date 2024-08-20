dba18987cd08db18d50eef480f0ef3f9cc495f56
goro-ne <hayao56@gmail.com>
2024-08-20 09:36:03 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div></div>
+      <p className="read-the-docs">React API test</p>
+    </>
+  );
+}
+
+export default App;
